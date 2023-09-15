a193ceb8943b1b1465187e752fdfd66c0cf34982
Rohan-SSS <singhrohan2533@gmail.com>
2023-09-15 20:06:20 +0530

    fixes

--- mapMarkers.py
+++ mapMarkers.py
@@ -1,9 +1,9 @@
 areas = [
-    {"center": {"latitude": 28.796910, "longitude": 77.538396}, "radius": 5},
+    {"center": {"latitude": 28.797355, "longitude": 77.53686}, "radius": 40},
     {"center": {"latitude": 28.795077, "longitude": 77.54062}, "radius": 10},
     {"center": {"latitude": 28.796864, "longitude": 77.53910}, "radius": 20},
-    {"center": {"latitude": 28.796398, "longitude": 77.541367}, "radius": 30},
-    {"center": {"latitude": 28.79791, "longitude": 77.540342}, "radius": 30}
+    {"center": {"latitude": 28.796398, "longitude": 77.54136}, "radius": 30},
+    {"center": {"latitude": 28.797915, "longitude": 77.54034}, "radius": 30}
 ]
 
 def mapMarkers():
